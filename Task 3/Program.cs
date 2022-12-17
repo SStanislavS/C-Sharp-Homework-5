cc25c5f40c3b69a73cde49bf0d93896d6e11ecd4
Stanislav <S@mail.ru>
2022-12-17 12:39:22 +0300

    add Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.Write("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+        {
+            max = numbers[j];
+        }
+    if (numbers[j] < min)
+        {
+            min = numbers[j];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = " + max);
+Console.WriteLine($"Минимальное значение = " + min);
+Console.WriteLine($"Разница между максимальным и минимальным значением = " + (max - min));
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(1,1000)) / 10;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
